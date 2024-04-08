f8e9aab4b2c9724ce2bd708de4f1056aefb39d67
azmataliakbar <azmat36@yahoo.com>
2024-04-08 17:27:03 +0500

    corrected sentence only

--- index.ts
+++ index.ts
@@ -82,7 +82,7 @@ console.log("\n",);
 
 console.log(chalk.italic.bold.underline.rgb(250, 245, 245)("Base value $1 = the rate of currency you're converting from :", fromAmmount));
 console.log(chalk.italic.bold.underline.rgb(250, 245, 245)("Base value $1 = the rate of currency you want to convert to:", toAmmount));
-console.log(chalk.italic.bold.underline.rgb(250, 245, 245)("Your asked the amount to convert :", amount));
+console.log(chalk.italic.bold.underline.rgb(250, 245, 245)("Your converted amount is :", amount));
 
 console.log("\n",);
 
